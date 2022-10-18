f04e9c60f8a2366933ff91c21df6713ed191e741
Mihray <2636853047@qq.com>
2022-10-18 17:41:16 +0800

    Create HomeVue.vue

---
+++ src/components/HomeVue.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>hello App!</h1>
+        <p>
+            <router-link to="/">Go to Home</router-link>
+            <router-link to="/about">Go to About</router-link>
+        </p>
+
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+// import vue from 'https://unpkg.com/vue@3';
+// import vuerouter from 'https://unpkg.com/vue-router@4';
+export default {
+    
+}
+</script>
