a85c09c6c77a521c256da19d6798e501f059aeb3
Mihray <2636853047@qq.com>
2022-10-21 10:55:12 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <!-- <HomeVue></HomeVue> -->
   <LoginVue></LoginVue>
+
+  <!-- <ul>
+   <router-link to='/LoginVue' ><a href="/LoginVue">LoginVue</a></router-link>
+   <router-link to='/HomeVue' ><a href="/HomeVue">HomeVue</a></router-link>
+ </ul> -->
+
 </template>
 
 <script>
@@ -17,5 +23,8 @@ export default {
 </script>
 
 <style>
-
+body{
+  background:#409EFF;
+  margin: 0;
+}
 </style>
